f80ffbf1383aa0bfe97168fcb0c2153f3176d62e
khoi <khoiracle@gmail.com>
2018-09-29 13:36:11 +0700

    💰 huge pile of cash

---
+++ SimpleBudget/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  SimpleBudget
+//
+//  Created by khoi on 9/29/18.
+//  Copyright © 2018 khoi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
