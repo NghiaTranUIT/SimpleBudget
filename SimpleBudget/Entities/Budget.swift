3f77590cbe3a28e183c8cfb62c4e8f03613c6cac
khoi <khoiracle@gmail.com>
2018-10-02 21:46:02 +0700

    Trim down budget

--- SimpleBudget/Entities/Budget.swift
+++ SimpleBudget/Entities/Budget.swift
@@ -12,9 +12,6 @@ import RealmSwift
 class Budget: Object {
   @objc dynamic var id = UUID().uuidString
   @objc dynamic var name = ""
-  @objc dynamic var amount: Int = 0
   @objc dynamic var currency = ""
-  @objc dynamic var startDate = Date(timeIntervalSinceNow: 0)
-  @objc dynamic var endDate: Date?
   let spends = List<Spend>()
 }
